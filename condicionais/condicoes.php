9d34f0d0c61c9df3e20574291b456a1f92411a0f
Ariel Santos <ariel.santosti@gmail.com>
2022-09-16 00:40:41 -0300

    Atualizando arquivos

---
+++ condicionais/condicoes.php
@@ -0,0 +1,19 @@
+<?php
+
+$num1 = 10;
+$num2 = 10;
+
+if($num1 > $num2)
+{
+    echo "$num1 é maior que $num2";
+
+}elseif($num1 == $num2){
+
+    echo "$num1 é igual a $num2";
+
+}else{
+
+    echo "$num1 não é maior que $num2";
+}
+
+?>
